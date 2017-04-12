db9a6b8825cd468dfb8dbad27a4c0a8abec30c06
Charliechen1 <Charliechen_1@163.com>
2017-04-12 19:14:42 +0800

    version 1.1

--- task3.sh
+++ task3.sh
@@ -1,31 +1,24 @@
-./main_para mushroom.csv 0.0002 1 1 1 20 x x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 x x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 x x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 x x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 x x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 s x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 s x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 s x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 s x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 s x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 t x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 t x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 t x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 t x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 t x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 p x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 p x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 p x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 p x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 p x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 n x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 n x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 n x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 n x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 n x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 e x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 e x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 e x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 e x mushtemp;
-./main_para mushroom.csv 0.0002 1 1 1 20 e x mushtemp;
+for i in {6..10}
+do
+	./main_para mushroom.csv 0.0002 1 $i $i 20 s x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 s x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 s x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 s x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 s x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 p x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 p x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 p x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 p x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 p x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 n x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 n x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 n x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 n x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 n x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 e x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 e x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 e x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 e x mushtemp;
+	./main_para mushroom.csv 0.0002 1 $i $i 20 e x mushtemp;
+done
 
